ebb1488fe021441ad2681f2ea70923548a13fc5a
dreamerLiang <506425878@qq.com>
2018-12-03 15:29:14 -0500

    ‘wordEmbedding1’

--- wordSenseDisambiguity.py
+++ wordSenseDisambiguity.py
@@ -1,39 +1,63 @@
 from sklearn.cluster import DBSCAN, AffinityPropagation, MeanShift
 from sklearn.mixture import GaussianMixture
 from gensim.models import Word2Vec
+import os
 from sklearn.preprocessing import  StandardScaler
 import sys
 import nltk
 import csv
 import string
 import numpy as np
+import re
+import glob
 
 
-def readCsvFile(file):
-    listOfRawText = []
-    with open(file, 'r') as csvFile:
-        csvReader = csv.reader(csvFile, delimiter = '\t')
-        for row in csvReader:
-            listOfRawText.append(row)
-    csvFile.close()
-    #for row in listOfSentence:
-    #    print (row)
-    return listOfRawText
+# def readCsvFile(directory):
+#     listOfRawText = []
+#     with open(file, 'r') as csvFile:
+#         csvReader = csv.reader(csvFile, delimiter = '\t')
+#         for row in csvReader:
+#             listOfRawText.append(row)
+#     csvFile.close()
+#     #for row in listOfSentence:
+#     #    print (row)
+#     return listOfRawText
 
+def readTsvDirectroy(path):
+    corpusDict = {}
+    for filename in glob.glob(os.path.join(path, '*.tsv')):
+        with open(filename, 'r') as tsvFile:
+            keyWord = re.split(r'/|\.' , filename)[1]
+            print (keyWord)
+            corpusDict[keyWord] = []
+            #tsvReader = csv.reader(tsvFile, delimiter='\t')
+            fileContent = tsvFile.readlines()
+            for i in range(1, len(fileContent)):
+                fileContent[i] = fileContent[i].rstrip('\n')
+                splittedPart = fileContent[i].split('\t')
+                #print (splittedPart)
+                corpusDict[keyWord].append(splittedPart)
+    #for key, val in corpusDict.items():
+    #    print (key, val)
+    return corpusDict
 
 def decomposeRawText(listOfRawText):
     #decompose text
     sentencesList = []
     eachSentence = []
+    #for i in range(len(listOfRawText)):
     for i in range(300):
+
         eachSentence.append(listOfRawText[i])
         if i % 3 == 2:
             sentencesList.append(eachSentence)
             eachSentence = []
     return sentencesList
 
-
-def preprocessing(sentencesList, stopWordsFileName, removeStopWords, removePunctuation):
+def preprocessing(corpusDict, key, stopWordsFileName, removeStopWords, removePunctuation):
+    #sentenceList = listOfRawText[:, 4]
+    sentenceList = [rawText[4] for rawText in corpusDict[key]]
+    #print (sentenceList)
     resList = list()
     stopWords = []
     if removeStopWords:
@@ -41,39 +65,60 @@ def preprocessing(sentencesList, stopWordsFileName, removeStopWords, removePunct
         stopWordsFile = infile.read().replace("\n", " ")
         stopWords = nltk.word_tokenize(stopWordsFile)
 
-    for i in range(len(sentencesList)):
-        sentence = ""
-        for word in sentencesList[i][0]:
-            sentence += word + " "
-        # print (sentence)
-
-        wordTokens = nltk.word_tokenize(sentence)
+    for i in range(len(sentenceList)):
+        #print (sentence)
+        wordTokens = nltk.word_tokenize(sentenceList[i])
         if removeStopWords:
-            wordTokens = [word for word in wordTokens if not word in stopWords]
+            wordTokens = [word for word in wordTokens if not word.lower() in stopWords]
         if removePunctuation:
             wordTokens = [word for word in wordTokens if not word in string.punctuation]
         resList.append(wordTokens)
-    return resList
 
+    for i in range(len(corpusDict[key])):
+        corpusDict[key][i][4] = resList[i]
+    #print (corpusDict[key])
+    return key, corpusDict[key]
 
-def getWord2VecEmbedding(sentencesList):
-    model = Word2Vec(sentencesList, min_count = 1)
+def getWord2VecEmbedding(key, processedList):
+    list1 = [rawText[4] for rawText in processedList]
+    inputList = []
+
+    for i in range(len(list1)):
+        index1 =list1[i].index(processedList[i][3])
+        print (index1)
+
+        eachInput = []
+        for j in range(index1 - 2, index1 + 3):
+            if j >= 0 and j < len(list1[i]):
+                eachInput.append(list1[i][j])
+            else:
+                eachInput.append('')
+        inputList.append(eachInput)
+
+    print(inputList)
+    model = Word2Vec(inputList, min_count = 1)
     words = sorted(model.wv.vocab.keys())
     res = StandardScaler().fit_transform([model[w] for w in words])
-    print (res)
-    return res
+    for ele in res:
+        print(ele)
 
+    print (len(res))
+    print (len(list1))
+
+    return key, res
 
 def applyCategorizationModel(data):
-    clt = DBSCAN(eps = 11.5, min_samples = 1).fit(data)
-    print (clt.labels_)
+    clt = DBSCAN(eps = 20, min_samples = 1).fit(data)
+    for ele in clt.labels_:
+        print(ele)
 
 
-filePath = sys.argv[1]
-listOfRawText = readCsvFile(filePath)
-sentencesList = decomposeRawText(listOfRawText)
-sentencesList = preprocessing(sentencesList, 'stoplist.txt', True, True)
-res = getWord2VecEmbedding(sentencesList)
-print("!!!!!!!!!!!!!!")
+
+
+
+directoryPath = sys.argv[1]
+corpusDict = readTsvDirectroy(directoryPath)
+# sentencesList = decomposeRawText(listOfRawText)
+key, processedList = preprocessing(corpusDict,'number', 'stoplist.txt', True, True)
+key, res = getWord2VecEmbedding(key, processedList)
 applyCategorizationModel(res)
-print("end!")
